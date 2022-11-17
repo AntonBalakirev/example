4b566cf3a64ae43faaa5254d2f994b9fe294a156
anton_balakirev <anton_balakirev@epage.ru>
2022-11-17 21:32:17 +0300

    update with lesson 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world! \u2764\u2764\u2764");
-        System.out.println("Hello world! \u2764\u2764\u2764");
+        String line = "Введите число: ";
+        System.out.println(line);
+        Scanner in = new Scanner(System.in);
+        String number = in.nextLine();
+        Integer intNumber = Integer.parseInt(number);
+        if (intNumber == 5){
+            System.out.println(intNumber + intNumber);
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
